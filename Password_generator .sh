6708423b15c69dc1b572ed6725c5cd625884f99a
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2020-05-14 12:39:56 +0300

    Update Password_generator .sh

--- Password_generator .sh
+++ Password_generator .sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 #Un simplu generator de string pentru parole
-#plictiseala 
+#plictiseala
 #in caz ce nu stii ce parola sa-ti pui
 #Dani
 echo "Hello i am password generator, i will provide a safe password"
 sleep 2
-echo "..."
+echo "hmm"
 sleep 2
 echo "We start to generted password"
-head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '' 
- if [ $? -ne 0 ]; then 
+echo "........................................."
+head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''
+ if [ $? -ne 0 ]; then
   echo "hmm ... sorry try agian"
   exit 1
  else
-  sleep 2
-  echo "bye"
+  echo "........................................."
+  echo "DONE!!!"
+  sleep 3
+  echo "bye!"
  fi
