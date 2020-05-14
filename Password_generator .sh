4a4c18658d6a0ffef57f57c31fc9eb8b731cba7d
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2020-05-14 12:29:34 +0300

    Update Password_generator .sh

--- Password_generator .sh
+++ Password_generator .sh
@@ -3,3 +3,19 @@
 #plictiseala 
 #in caz ce nu stii ce parola sa-ti pui
 #Dani
+echo "Hello i am password generator, i will provide a safe password"
+sleep 2
+echo "..."
+sleep 2
+echo "We start to generted password"
+head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '' > password.txt
+ if [ $? -ne 0 ]; then 
+  echo "hmm ... sorry try agian"
+  exit 1
+ else
+  echo "DONE!!!"
+  echo "You password is in password.txt"
+  echo password.txt
+  sleep 3
+  echo "buy"
+ fi
